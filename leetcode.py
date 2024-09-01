a1ccef6b79cac8003e92aa8667eebe4c49cec3d6
Nga Vu <134727438+zlnga1001@users.noreply.github.com>
2024-09-01 15:25:02 -0400

    added

---
+++ leetcode.py
@@ -0,0 +1,16 @@
+def lineup(artists, set_times):
+    thisDict = {}
+    #len(artist) = 4
+    #range(len(artist)) = [0,1,2,3]
+    for i in range(len(artists)):
+            thisDict[artists[i]] = set_times[i]
+    return thisDict
+
+artists1 = ["Kendrick Lamar", "Chappell Roan", "Mitski", "Rosalia"]
+set_times1 = ["9:30 PM", "5:00 PM", "2:00 PM", "7:30 PM"]
+
+artists2 = []
+set_times2 = []
+
+print(lineup(artists1, set_times1))
+print(lineup(artists2, set_times2))
